d0241c5bffaed5b94abf8491b41e230f118c366f
siskaendah <18102033@ittelkom-pwt.ac.id>
2021-09-24 09:21:43 +0700

    checkpoint

--- app/src/main/java/com/bpsbanyumas/onetouchstatisticsbanyumas/MainActivity.kt
+++ app/src/main/java/com/bpsbanyumas/onetouchstatisticsbanyumas/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bpsbanyumas.onetouchstatisticsbanyumas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bpsbanyumas.onetouchstatisticsbanyumas.adapters.MainDataAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,10 +10,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     
     private val list = ArrayList<MainData>()
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         rv_maindata.setHasFixedSize(true)
         list.addAll(getListMainData())
