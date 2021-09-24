2ec810eb51a485f6f66b805d6a862c499c5124ed
retavitantri <retavitan@gmail.com>
2021-09-24 15:17:33 +0700

    menambahkan clicklistener dan test activity baru

--- app/src/main/java/com/bpsbanyumas/onetouchstatisticsbanyumas/adapters/MainDataAdapter.kt
+++ app/src/main/java/com/bpsbanyumas/onetouchstatisticsbanyumas/adapters/MainDataAdapter.kt
@@ -2,14 +2,18 @@ package com.bpsbanyumas.onetouchstatisticsbanyumas.adapters
 
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bpsbanyumas.onetouchstatisticsbanyumas.DetailActivity
 import com.bpsbanyumas.onetouchstatisticsbanyumas.MainData
 import com.bpsbanyumas.onetouchstatisticsbanyumas.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.executor.GlideExecutor.UncaughtThrowableStrategy.LOG
 import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.card_item.view.*
 
@@ -25,14 +29,17 @@ class MainDataAdapter(private val listMyData: ArrayList<MainData>, val context: 
 
         val myData = listMyData[position]
 
-        holder.itemView.setOnClickListener {
-
+        holder.cv.setOnClickListener {
+            context.startActivity(Intent(context, DetailActivity::class.java))
+            Log.d(TAG, position.toString())
         }
     }
 
     override fun getItemCount(): Int = listMyData.size
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cv: CardView = itemView.findViewById(R.id.cv_item)
+
         fun bind(myData: MainData) {
             with(itemView){
                 Glide.with(itemView.context).load(myData.icon).apply(RequestOptions().override(500, 500)).into(iv_icon)
