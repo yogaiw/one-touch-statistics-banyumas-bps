2b1ddeefe9ba23b30c185fb4ff8e56e1dd4d0e92
muhamaz <humamzaky7@gmail.com>
2021-09-24 14:53:28 +0700

    Change adapters

--- app/src/main/java/com/bpsbanyumas/onetouchstatisticsbanyumas/adapters/MainDataAdapter.kt
+++ app/src/main/java/com/bpsbanyumas/onetouchstatisticsbanyumas/adapters/MainDataAdapter.kt
@@ -1,41 +1,45 @@
 package com.bpsbanyumas.onetouchstatisticsbanyumas.adapters
 
+import android.content.ContentValues.TAG
 import android.content.Context
-import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bpsbanyumas.onetouchstatisticsbanyumas.MainData
 import com.bpsbanyumas.onetouchstatisticsbanyumas.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.card_item.view.*
 
-class MainDataAdapter(private val listMyDatas: ArrayList<MainData>, val context: Context) : RecyclerView.Adapter<MainDataAdapter.CardViewViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
-        return CardViewViewHolder(view)
-    }
-    override fun onBindViewHolder(holder: CardViewViewHolder, position: Int) {
-        val mainData = listMyDatas[position]
+class MainDataAdapter(private val listMyData: ArrayList<MainData>, val context: Context) : RecyclerView.Adapter<MainDataAdapter.ListViewHolder>() {
 
-        Glide.with(holder.itemView.context).load(mainData.icon).apply(RequestOptions().override(350, 550)).into(holder.ivIcon)
-        holder.tvJudul.text = mainData.judul
-        holder.tvValue.text = mainData.value
-        holder.tvDeskripsi.text = mainData.deskripsi
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
+        return ListViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return listMyDatas.size
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listMyData[position])
+
+        val myData = listMyData[position]
+
+        holder.itemView.setOnClickListener {
+
+        }
     }
 
-    inner class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var ivIcon: ImageView = itemView.findViewById(R.id.iv_icon)
-        var tvJudul: TextView = itemView.findViewById(R.id.tv_judul)
-        var tvValue: TextView = itemView.findViewById(R.id.tv_value)
-        var tvDeskripsi: TextView = itemView.findViewById(R.id.tv_deskripsi)
+    override fun getItemCount(): Int = listMyData.size
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(myData: MainData) {
+            with(itemView){
+                Glide.with(itemView.context).load(myData.icon).apply(RequestOptions().override(500, 500)).into(iv_icon)
+                tv_judul.text = myData.judul
+                tv_value.text = myData.value
+                tv_deskripsi.text = myData.deskripsi
+            }
+        }
     }
 }
